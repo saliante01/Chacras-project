640107f3ba4c661f07be4f0dfe7359a3637552b3
saliante01 <s.aliante01@ufromail.cl>
2025-10-20 23:54:26 -0300

    Update web config

--- chacras-backend/src/main/java/com/example/chacrasbackend/security/WebConfig.java
+++ chacras-backend/src/main/java/com/example/chacrasbackend/security/WebConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    //archivo de configuración
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
